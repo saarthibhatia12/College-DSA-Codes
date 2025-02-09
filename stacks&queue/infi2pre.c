0a69b25ae7d02296cd6e827d1415bd4585645c99
RahulRR-10 <155348604+RahulRR-10@users.noreply.github.com>
2025-02-09 16:23:34 +0530

    Update infi2pre.c

--- stacks&queue/infi2pre.c
+++ stacks&queue/infi2pre.c
@@ -1,163 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
+#include <math.h>
+#define MAX 100
 
-struct stack
-{
-    int size;
+typedef struct stack{
     int top;
-    char *arr;
-};
+    char item[MAX];
+}STACK;
 
-int stackTop(struct stack* sp){
-    return sp->arr[sp->top];
+void push(STACK *st,char ele){
+    if(st->top==MAX-1) return;
+    st->item[++(st->top)] = ele;
 }
 
-int isEmpty(struct stack *ptr)
-{
-    if (ptr->top == -1)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+void pop(STACK *st){
+    if(st->top==-1) return;
+    st->top--;
 }
 
-int isFull(struct stack *ptr)
-{
-    if (ptr->top == ptr->size - 1)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+int isempty(STACK *st){
+    if(st->top==-1) return 1;
+    else return 0;
 }
 
-void push(struct stack* ptr, char val){
-    if(isFull(ptr)){
-        printf("Stack Overflow! Cannot push %c to the stack\n", val);
-    }
-    else{
-        ptr->top++;
-        ptr->arr[ptr->top] = val;
+void ini(STACK *st){
+    st->top=-1;
+}
+
+int prio(char c){
+    switch(c){
+        case '^' : return 3;
+        case '*':
+        case '/':return 2;
+        case '+':
+        case '-':return 1;
+        default : return -1;
     }
 }
 
-char pop(struct stack* ptr){
-    if(isEmpty(ptr)){
-        printf("Stack Underflow! Cannot pop from the stack\n");
-        return -1;
-    }
-    else{
-        char val = ptr->arr[ptr->top];
-        ptr->top--;
-        return val;
-    }
-}
-
-int precedence(char ch){
-    if(ch == '*' || ch=='/')
-        return 3;
-    else if(ch == '+' || ch=='-')
-        return 2; 
-    else
-        return 0;
-}
-
-int isOperator(char ch){
-    if(ch=='+' || ch=='-' ||ch=='*' || ch=='/'||ch=='^') 
-        return 1;
-    else
-        return 0;
-}
-
-void reverseString(char* exp) {
-    int n = strlen(exp);
-    for (int i = 0; i < n / 2; i++) {
-        char temp = exp[i];
-        exp[i] = exp[n - i - 1];
-        exp[n - i - 1] = temp;
+void reverse(char *a){
+    int n = strlen(a);
+    int start = 0;
+    int end = n-1;
+    while(start<end){
+        char temp = a[start];
+        a[start] = a[end];
+        a[end] = temp;
+        start++;
+        end--;
     }
 }
 
-void replaceParentheses(char* exp) {
-    for (int i = 0; exp[i] != '\0'; i++) {
-        if (exp[i] == '(') {
-            exp[i] = ')';
-        } else if (exp[i] == ')') {
-            exp[i] = '(';
+void conv(char *s){
+    STACK st;
+    ini(&st);
+    char ans[MAX] = "";
+    int k = 0;
+    int n = strlen(s);
+    reverse(s);
+    for(int i=0;i<n;i++){
+        if((s[i]>='a'&&s[i]<='z')||(s[i]>='A'&&s[i]<='Z')||(s[i]>='0'&&s[i]<='9')){
+            ans[k++] = s[i];
         }
-    }
-}
-
-char* infixToPostfix(char* infix){
-    struct stack * sp = (struct stack *) malloc(sizeof(struct stack));
-    sp->size = 10; 
-    sp->top = -1;
-    sp->arr = (char *) malloc(sp->size * sizeof(char));
-    char * postfix = (char *) malloc((strlen(infix)+1) * sizeof(char));
-    int i=0; // Track infix traversal
-    int j = 0; // Track postfix addition 
-    while (infix[i]!='\0')
-    {
-        if(!isOperator(infix[i]) && infix[i] != '(' && infix[i] != ')'){
-            postfix[j] = infix[i];
-            j++;
-            i++;
+        else if(s[i]==')'){
+            push(&st,s[i]);
         }
-        else if (infix[i] == '(') {
-            push(sp, '(');
-            i++;
-        }
-        else if (infix[i] == ')') {
-            while (!isEmpty(sp) && stackTop(sp) != '(') {
-                postfix[j] = pop(sp);
-                j++;
+        else if(s[i]=='('){
+            while(!isempty(&st)&&st.item[st.top]!=')'){
+                ans[k++] = st.item[st.top];
+                pop(&st);
             }
-            pop(sp); // pop '('
-            i++;
+            pop(&st);
         }
         else{
-            while (!isEmpty(sp) && precedence(infix[i]) <= precedence(stackTop(sp))) {
-                postfix[j] = pop(sp);
-                j++;
+            while(!isempty(&st)&&prio(st.item[st.top])>prio(s[i])){
+                ans[k++] = st.item[st.top];
+                pop(&st);
             }
-            push(sp, infix[i]);
-            i++;
+            push(&st,s[i]);
         }
     }
-    while (!isEmpty(sp))    
-    {
-        postfix[j] = pop(sp);
-        j++;
+    while(!isempty(&st)){
+        ans[k++]=st.item[st.top];
+        pop(&st);
     }
-    postfix[j] = '\0';
-    return postfix;
+    reverse(ans);
+    printf("%s",ans);
 }
 
-char* infixToPrefix(char* infix) {
-    // Step 1: Reverse the infix expression
-    reverseString(infix);
-
-    // Step 2: Replace '(' with ')' and vice versa
-    replaceParentheses(infix);
-
-    // Step 3: Get the postfix of the modified expression
-    char* postfix = infixToPostfix(infix);
-
-    // Step 4: Reverse the postfix to get the prefix
-    reverseString(postfix);
-
-    return postfix;
-}
-
-int main()
-{
-    char infix[] = "x-y/z-k*d";
-    printf("Prefix is %s", infixToPrefix(infix));
-    return 0;
+int main(){
+    char s[100] = "-K+L-M*N+(0^P)*W/U/V*T+Q";
+    conv(s);
 }
